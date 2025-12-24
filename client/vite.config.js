6d8561d5a5bf3653682b392064d3f62e986d726e
IftikharZahid <IftikharXahid@gmail.com>
2025-12-25 00:33:00 +0500

    deploy script added

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/PGC-Website/', // Replace 'PGC-Website' with your GitHub repository name
   plugins: [react()],
   server: {
     proxy: {
